1beee7b16aafc4f705564fbabb047beee1cdfc8d
andresantiagohellmann <andresantiagohellmann@gmail.com>
2020-03-25 23:59:45 -0300

    chore add SecExpoDubai

--- src/views/Home/SecExpoDubai/index.vue
+++ src/views/Home/SecExpoDubai/index.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="SecExpoDubai">SecExpoDubai</div>
+  <div class="SecExpoDubai">
+    <div class="SecExpoDubai__container">
+      <div class="SecExpoDubai__col">
+        <div class="SecExpoDubai__figure">
+          <img
+            src="@/assets/images/expo2020-call.jpg"
+            alt="O que é a Expo 2020?"
+            class="SecExpoDubai__image"
+          />
+        </div>
+      </div>
+      <div class="SecExpoDubai__col">
+        <div class="SecExpoDubai__content">
+          <h1 class="SecExpoDubai__content__title">
+            O que é a Expo 2020?
+          </h1>
+          <div class="SecExpoDubai__content__text">
+            call
+          </div>
+          <div class="SecExpoDubai__content__button">
+            <TheButton text="Saiba Mais" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import TheButton from "@/components/TheButton/";
+
 export default {
-  name: "secExpoDubai"
+  name: "secExpoDubai",
+  components: {
+    TheButton
+  }
 };
 </script>
 
