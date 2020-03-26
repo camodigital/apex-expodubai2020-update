9790d5ae32d14b927a2a8cc1d08eb815128a843e
andresantiagohellmann <andresantiagohellmann@gmail.com>
2020-03-26 00:45:50 -0300

    chore add SecSocial

--- src/views/Home/SecSocial/index.vue
+++ src/views/Home/SecSocial/index.vue
@@ -1,5 +1,127 @@
 <template>
-  <div class="secSocial">SecSocial</div>
+  <div class="secSocial">
+    <div class="secSocial__container">
+      <div class="secSocial__facebook">
+        <div class="secSocial__facebook__header">
+          <h1>facebook</h1>
+        </div>
+        <div class="secSocial__facebook__list">
+          <div class="secSocial__facebook__item">
+            <div class="secSocial__facebook__figure">
+              <img
+                src="@/assets/images/facebook1.jpg"
+                alt=""
+                class="secSocial__facebook__image"
+              />
+            </div>
+            <div class="secSocial__facebook__content">
+              <div class="secSocial__facebook__title">
+                <h2>title</h2>
+              </div>
+              <div class="secSocial__facebook__text">
+                text
+              </div>
+            </div>
+          </div>
+          <div class="secSocial__facebook__item">
+            <div class="secSocial__facebook__figure">
+              <img
+                src="@/assets/images/facebook2.jpg"
+                alt=""
+                class="secSocial__facebook__image"
+              />
+            </div>
+            <div class="secSocial__facebook__content">
+              <div class="secSocial__facebook__title">
+                <h2>title</h2>
+              </div>
+              <div class="secSocial__facebook__text">
+                text
+              </div>
+            </div>
+          </div>
+          <div class="secSocial__facebook__item">
+            <div class="secSocial__facebook__figure">
+              <img
+                src="@/assets/images/facebook3.jpg"
+                alt=""
+                class="secSocial__facebook__image"
+              />
+            </div>
+            <div class="secSocial__facebook__content">
+              <div class="secSocial__facebook__title">
+                <h2>title</h2>
+              </div>
+              <div class="secSocial__facebook__text">
+                text
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="secSocial__instagram">
+      <div class="secSocial__instagram__header">
+        <h1>instagram</h1>
+      </div>
+      <div class="secSocial__instagram__list">
+        <div class="secSocial__instagram__item">
+          <div class="secSocial__instagram__figure">
+            <img
+              src="@/assets/images/instagram1.jpg"
+              alt=""
+              class="secSocial__instagram__image"
+            />
+          </div>
+        </div>
+        <div class="secSocial__instagram__item">
+          <div class="secSocial__instagram__figure">
+            <img
+              src="@/assets/images/instagram2.jpg"
+              alt=""
+              class="secSocial__instagram__image"
+            />
+          </div>
+        </div>
+        <div class="secSocial__instagram__item">
+          <div class="secSocial__instagram__figure">
+            <img
+              src="@/assets/images/instagram3.jpg"
+              alt=""
+              class="secSocial__instagram__image"
+            />
+          </div>
+        </div>
+        <div class="secSocial__instagram__item">
+          <div class="secSocial__instagram__figure">
+            <img
+              src="@/assets/images/instagram4.jpg"
+              alt=""
+              class="secSocial__instagram__image"
+            />
+          </div>
+        </div>
+        <div class="secSocial__instagram__item">
+          <div class="secSocial__instagram__figure">
+            <img
+              src="@/assets/images/instagram5.jpg"
+              alt=""
+              class="secSocial__instagram__image"
+            />
+          </div>
+        </div>
+        <div class="secSocial__instagram__item">
+          <div class="secSocial__instagram__figure">
+            <img
+              src="@/assets/images/instagram6.jpg"
+              alt=""
+              class="secSocial__instagram__image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
