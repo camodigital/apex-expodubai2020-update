089aafe5cde0dda464524501cabcb91034a00f8c
andresantiagohellmann <andresantiagohellmann@gmail.com>
2020-03-29 02:15:07 -0300

    fix responsive secDesc Expodubai

--- src/views/ExpoDubai/SecDesc/index.vue
+++ src/views/ExpoDubai/SecDesc/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="secDesc">
     <div class="secDesc__container">
-      <div class="secDesc__content" v-html="$t('ExpoDubai.call')"></div>
+      <div
+        class="secDesc__content textCenter"
+        v-html="$t('ExpoDubai.call')"
+      ></div>
     </div>
   </div>
 </template>
